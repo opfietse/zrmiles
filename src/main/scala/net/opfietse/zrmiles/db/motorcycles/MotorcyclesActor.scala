ece4c71716d7f2eea547c5c600f8cc1a595d8744
Mark <rvk@macbook-air1.fritz.box>
2016-03-14 23:04:16 +0100

    Started over with motorcycles route

---
+++ src/main/scala/net/opfietse/zrmiles/db/motorcycles/MotorcyclesActor.scala
@@ -0,0 +1,99 @@
+package net.opfietse.zrmiles
+package db
+package motorcycles
+
+import scala.concurrent.Future
+import akka.actor._
+
+import slick.driver.MySQLDriver.api._
+
+import ZrMilesSchema._
+import model._
+import util.ActorRefFactorySupport
+
+object MotorcyclesActor {
+  def props = Props[MotorcyclesActor]
+  def name = "riders-actor"
+
+  sealed trait Command
+  case object GetAllMotorcycles extends Command
+  case object GetAllMotorcyclesWithRiders extends Command
+  case class GetMotorcyclesForRiderId(riderId: Int) extends Command
+}
+
+class MotorcyclesActor extends Actor with ActorLogging {
+  import MotorcyclesActor._
+
+  implicit val executionContext = context.dispatcher
+
+  def receive: Receive = {
+    case GetAllMotorcycles =>
+      getAllMotorcyclesWithRiders
+      val bikes = getAllMotorcycles
+      sender ! bikes
+    case GetAllMotorcyclesWithRiders =>
+      val bikes = getAllMotorcyclesWithRiders
+      sender ! bikes
+  }
+
+  def getAllMotorcycles: Future[Seq[Motorcycle]] = {
+    log.info("Run GetAllMotorcycles ++++++++++++")
+    val db = Database.forConfig("mysql")
+
+    try {
+      val dbMotorcycles = for (m <- motorcycles) yield m
+      val q = dbMotorcycles.result
+      val f: Future[Seq[Motorcycle]] = db.run(q)
+
+      val res = for (names <- f) yield names
+      res
+    } finally db.close
+  }
+
+  //  def getAllMotorcyclesWithRiders: Future[Seq[(Int, Int, String, String, Int, Int, String, String)]] = {
+  def getAllMotorcyclesWithRiders: Future[Seq[MotorcycleWithRider]] = {
+    //  def getAllMotorcyclesWithRiders = {
+    log.info("Run GetAllMotorcyclesWithRiders ++++++++++++")
+    val db = Database.forConfig("mysql")
+
+    try {
+      //      val joinQuery: Query[(Rep[String], Rep[String]), (String, String), Seq] = for {
+      //        c <- coffees if c.price > 9.0
+      //        s <- c.supplier
+      //      } yield (c.name, s.name)
+
+      //      val joinQuery: Query[(Rep[Int], Rep[Int], Rep[String], Rep[String], Rep[Int], Rep[Int], Rep[String], Rep[String]), (Int, Int, String, String, Int, Int, String, String), Seq] = for {
+      val joinQuery: Query[(Rep[Int], Rep[Int], Rep[String], Rep[String], Rep[Int], Rep[Int], Rep[String], Rep[String]), (Int, Int, String, String, Int, Int, String, String), Seq] = for {
+        m <- motorcycles
+        r <- m.rider
+      } yield (m.id, m.riderId, m.make, m.model, m.year, m.distanceUnit, r.firstName, r.lastName)
+
+      //      val dbMotorcycles = for (m <- motorcycles) yield m
+      val q = joinQuery.result
+      //      val f: Future[Seq[GetAllMotorcyclesWithRiders]] = db.run(q)
+      val f: Future[Seq[(Int, Int, String, String, Int, Int, String, String)]] = db.run(q)
+
+      //      f.map(println)
+      //      val res = for ((id: Int, riderId: Int, make: String, model: String, year: Int, distanceUnit: Int, firstName: String, lastName: String) <- f) yield MotorcycleWithRider(id, riderId, make, model, year, distanceUnit, firstName, lastName)
+      //      val res = f.map((id: Int, riderId: Int, make: String, model: String, year: Int, distanceUnit: Int, firstName: String, lastName: String) => MotorcycleWithRider(id, riderId, make, model, year, distanceUnit, firstName, lastName)
+      //    names
+
+      f.map(s => makeM(s))
+      //      val res = for (names <- f) yield names
+      //      res
+    } finally db.close
+  }
+
+  def makeM(s: Seq[(Int, Int, String, String, Int, Int, String, String)]): Seq[MotorcycleWithRider] = {
+    val res = for ((id: Int, riderId: Int, make: String, model: String, year: Int, distanceUnit: Int, firstName: String, lastName: String) <- s) yield MotorcycleWithRider(id, riderId, make, model, year, distanceUnit, firstName, lastName)
+    res
+  }
+}
+
+trait MotorcyclesActorProvider {
+  def motorcyclesActor: ActorRef
+}
+
+trait SlickMotorcyclesActorProvider extends MotorcyclesActorProvider with ActorRefFactorySupport {
+  lazy val motorcyclesActor = actorRefFactory.actorOf(MotorcyclesActor.props, MotorcyclesActor.name)
+}
---
+++ src/main/scala/net/opfietse/zrmiles/web/motorcycles/MotorcyclesRoute.scala
@@ -0,0 +1,89 @@
+package net.opfietse.zrmiles
+package web
+package motorcycles
+
+import scala.concurrent._
+import scala.util._
+
+import akka.actor._
+import akka.pattern.ask
+import spray.http.MediaTypes._
+import spray.http.StatusCodes
+import spray.routing.HttpService
+
+import org.slf4j.LoggerFactory
+
+import net.opfietse.zrmiles.db.motorcycles.SlickMotorcyclesActorProvider
+import net.opfietse.zrmiles.web.common
+import net.opfietse.zrmiles._
+import net.opfietse.zrmiles.model._
+import net.opfietse.zrmiles.web.common.CommonHtml._
+import db.motorcycles.MotorcyclesActor._
+import net.opfietse.zrmiles.util._
+
+trait MotorcyclesRoute extends HttpService
+    with ExecutionContextSupport
+    with ActorAskPattern
+    with SlickMotorcyclesActorProvider {
+
+  private def log = LoggerFactory.getLogger("RiderRoute")
+
+  val motorcyclesRoute = get {
+    path("bikes" / "bikes.jsp") {
+      pathEndOrSingleSlash {
+        onComplete((motorcyclesActor ? GetAllMotorcyclesWithRiders).mapTo[Future[Seq[MotorcycleWithRider]]].flatMap(motorcycles => motorcycles)) {
+          case Success(bikes) =>
+            val html = Header + "<BODY>" + Menu + "<BR/><BR/>All riders in the database.<BR/><BR />" +
+              makeMotorcyclesTableW(bikes) + instructions + "</BODY>" + Footer
+            respondWithMediaType(`text/html`) {
+              complete(html)
+            }
+
+          case Failure(t) =>
+            log.error("Error retreiving motorcycles", t)
+            respondWithMediaType(`text/html`) {
+              complete("Oops .....")
+            }
+        }
+      }
+    }
+  }
+
+  //  def makeRidersTable(riders: List[Riderr]): String = {
+  //    val riderTableEntries = for (r <- riders) yield "<td>" + r.id + "</td>" + "<td>" + r.firstName + "</td>"
+  //    "<table border=\"1\"> summary=\"Lists the riders currently in the database\"><CAPTION><EM>Riders</EM></CAPTION>" + riderTableEntries.mkString("<tr>", "</tr><tr>", "</tr>") + "</table>"
+  //  }
+
+  private val instructions = "<BR>Click on the <B>Id</B> to show/add the bikes for that rider.<BR>Click on the <B>last name</B> to CHANGE or DELETE that rider."
+
+  def makeMotorcyclesTable(motorcycles: Seq[Motorcycle]): String = {
+    val tableHeader = "<th>Id</th><th>Owner</th><th>Make</th><th>Model</th><th>Year</th><th>Odometer in</th><th>Current mileage</th>"
+    val motorcycleTableEntries = for (m <- motorcycles) yield "<td>" + makeMilesLink(m) + "</td>" +
+      "<td>" + m.riderId + "</td>" +
+      "<td>" + m.make + "</td>" +
+      "<td>" + m.model + "</td>" +
+      "<td>" + m.year + "</td>" +
+      "<td>" + m.distanceUnit + "</td>"
+
+    "<table border=\"1\" summary=\"Lists the bikes currently in the database\"><CAPTION><EM>Bikes</EM></CAPTION>" +
+      tableHeader + motorcycleTableEntries.mkString("<tr>", "</tr><tr>", "</tr>") + "</table>"
+  }
+
+  def makeMotorcyclesTableW(motorcycles: Seq[MotorcycleWithRider]): String = {
+    val tableHeader = "<th>Id</th><th>Owner</th><th>Make</th><th>Model</th><th>Year</th><th>Odometer in</th><th>Current mileage</th>"
+    val motorcycleTableEntries = for (m <- motorcycles) yield "<td>" + makeMilesLink(m) + "</td>" +
+      "<td>" + m.firstName + " " + m.lastName + "</td>" +
+      "<td>" + m.make + "</td>" +
+      "<td>" + m.model + "</td>" +
+      "<td>" + m.year + "</td>" +
+      "<td>" + m.distanceUnit + "</td>"
+
+    "<table border=\"1\" summary=\"Lists the bikes currently in the database\"><CAPTION><EM>Bikes</EM></CAPTION>" +
+      tableHeader + motorcycleTableEntries.mkString("<tr>", "</tr><tr>", "</tr>") + "</table>"
+  }
+
+  def makeMilesLink(motorcycle: Motorcycle) = "<a href=\"miles/showandadd.jsp?motorcycleId=" + motorcycle.id + "\">" + motorcycle.id + "</a>"
+  def makeMilesLink(motorcycle: MotorcycleWithRider) = "<a href=\"miles/showandadd.jsp?motorcycleId=" + motorcycle.id + "\">" + motorcycle.id + "</a>"
+  //  def makeAddBikeLink(rider: Rider) = "<a href=\"bikes/add.jsp?riderId=" + rider.id + "\">" + rider.id + "</a>"
+  //  def makeUpdateRiderLink(rider: Rider) = "<a href=\"riders/update.jsp?riderId=" + rider.id + "\">" + rider.lastName + "</a>"
+}
